aa44d9be8ef2170086c40e548a90c0d3fa82aee3
John Johansen <johnjohansendev@gmail.com>
2022-04-23 19:30:53 -0400

    CRA with Tailwind boilerplate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container min-w-full bg-zinc-100">
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-5xl subpixel-antialiased font-bold">
+          React CRA with tailwind boilerplate
+        </h1>
+      </div>
     </div>
   );
 }
